a3d5c3edee7bc76504a272ee48b5326b46f77ae4
Wilfredo Ramirez <rwilfredo534@gmail.com>
2020-06-20 05:37:01 -0400

    revised_commit

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const aElement = document.createElement('A')
 imgElement.classList.add('image')
 //imgElement.img = src;"https://www.shutterstock.com/image-photo/cannes-france-may-15-2018-chewbacca-1227212848" ;alt="Simply Easy Learning" ;width="200"
 //height="80" 
-document.body.appendChild(imgElement)
+//document.body.appendChild(imgElement)
 
 aElement.classList.add('link')
 buttonElement.append('remove')
